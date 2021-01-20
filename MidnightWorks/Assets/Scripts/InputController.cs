57875ba37d6d0002b188a41ed68becc98387a002
sasha <sahsagrom1999@gmail.com>
2021-01-20 23:19:55 +0200

    added simple input system

---
+++ MidnightWorks/Assets/Scripts/InputController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField] private float smoothTime;
+    [SerializeField] private float leftRightMaxPosition;
+    [SerializeField] private float movingSpeed;
+
+    private Vector3 velocity;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * movingSpeed);
+
+        if (Input.GetMouseButton(0))
+        {
+            float inputP = Input.mousePosition.x / Screen.width * 2 - 1;
+            Vector3 targetPosition = new Vector3(inputP * leftRightMaxPosition, transform.position.y, transform.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime * Time.deltaTime);
+        }
+    }
+}
